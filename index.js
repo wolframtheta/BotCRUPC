db8f2003141cbd8fa2f827220540b5fd06997b11
Xavier Marques <xaviermarques4f@gmail.com>
2017-10-03 10:18:55 +0000

    Add read and write file

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-const Telegraf = require('telegraf')
+var Telegraf = require('telegraf')
+var fs = require('fs')
+var path = require('path');
+
 
 const app = new Telegraf("457148721:AAH59U4s3FEVqiE_V43koSUSG0QEVZVp_HI")
-app.command('start', ({ from, reply }) => {
-  console.log('start', from)
-  return reply('Welcome!')
+app.command('isopen', ({ from, reply }) => {
+  var file = fs.readFileSync("stateCRUPC", 'utf-8');
+  var isopen = (file == "open") ? true : false;
+  if (isopen) reply("El club esta abierto");
+  else reply("El club esta cerrado");
 })
 app.hears('hi', (ctx) => ctx.reply('Hey there!'))
 app.on('sticker', (ctx) => {
+  var file = fs.readFileSync("stateCRUPC", 'utf-8');
   if (ctx.message.sticker.emoji == '🚪' && ctx.message.sticker.set_name == "CRUPC") {
+    console.log("file:" + file);
+    if (file == "closed") {
+      fs.writeFileSync("stateCRUPC", "open");
+      console.log("write");
+    }
     console.log("open");
+    ctx.reply('Club abierto')
   }
-  if (ctx.message.sticker.emoji == '😒' && ctx.message.sticker.set_name == "CRUPC") {
-    console.log("close");
+  else if (ctx.message.sticker.emoji == '😒' && ctx.message.sticker.set_name == "CRUPC") {
+    if (file == "open") {
+      fs.writeFileSync("stateCRUPC", "closed");
+      console.log("write");
+    }
+    console.log("closed");
+    ctx.reply('Club cerrado')
   }
-  ctx.reply('👍')
 })
 app.startPolling()
