abf2263df684884135c21558196e35a4c688e459
Xavier Marques <xaviermarques4f@gmail.com>
2017-10-04 21:06:25 +0200

    Add replies with stickers

--- index.js
+++ index.js
@@ -4,31 +4,43 @@ var path = require('path');
 
 
 const app = new Telegraf("457148721:AAH59U4s3FEVqiE_V43koSUSG0QEVZVp_HI")
-app.command('isopen', ({ from, reply }) => {
+app.command('isopen', (ctx) => {
   var file = fs.readFileSync("stateCRUPC", 'utf-8');
   var isopen = (file == "open") ? true : false;
-  if (isopen) reply("El club esta abierto");
-  else reply("El club esta cerrado");
+  if (isopen) ctx.telegram.sendSticker(ctx.message.chat.id, 'CAADBAADRgADZhkVBXvnbcBrbN7EAg');
+  else ctx.telegram.sendSticker(ctx.message.chat.id, 'CAADBAADSAADZhkVBR-6hyRG6fjYAg');
 })
-app.hears('hi', (ctx) => ctx.reply('Hey there!'))
-app.hears('sticker', (ctx) => {
-  var file = fs.readFileSync("stateCRUPC", 'utf-8');
-  if (ctx.message.sticker.emoji == '🚪' && ctx.message.sticker.set_name == "CRUPC") {
-    console.log("file:" + file);
-    if (file == "closed") {
-      fs.writeFileSync("stateCRUPC", "open");
-      console.log("write");
-    }
-    console.log("open");
-    ctx.reply('Club abierto')
+app.hears('Hi', (ctx) => ctx.reply('Holiiiiiiiiiiiiiiii!!'))
+app.hears('hi', (ctx) => ctx.reply('Holiiiiiiiiiiiiiiii!!'))
+app.on('text', (ctx) => {
+  if (ctx.message.chat.id > 0 && ctx.message.from.username != 'XvaiKawaii') {
+    ctx.reply('A ti no te hago ni puto caso');
   }
-  else if (ctx.message.sticker.emoji == '😒' && ctx.message.sticker.set_name == "CRUPC") {
-    if (file == "open") {
-      fs.writeFileSync("stateCRUPC", "closed");
-      console.log("write");
+})
+app.on('sticker', (ctx) => {
+
+  if (ctx.message.chat.id < 0 || (ctx.message.chat.id > 0 && ctx.message.from.username == 'XvaiKawaii')) {
+    var file = fs.readFileSync("stateCRUPC", 'utf-8');
+    if (ctx.message.sticker.emoji == '🚪' && ctx.message.sticker.set_name == "CRUPC") {
+      console.log("file:" + file);
+      if (file == "closed") {
+        fs.writeFileSync("stateCRUPC", "open");
+        console.log("write");
+      }
+      console.log("open");
+      ctx.reply('Club abierto')
     }
-    console.log("closed");
-    ctx.reply('Club cerrado')
+    else if (ctx.message.sticker.emoji == '😒' && ctx.message.sticker.set_name == "CRUPC") {
+      if (file == "open") {
+        fs.writeFileSync("stateCRUPC", "closed");
+        console.log("write");
+      }
+      console.log("closed");
+      ctx.reply('Club cerrado')
+    }
+  } 
+  else {
+    ctx.reply('A ti no te hago ni puto caso');
   }
 })
 app.startPolling()
