5910214670e605d1005a88d3df750d9af0982e57
Przemek Lewandowski <devanist@outlookc.com>
2017-10-05 10:23:21 +0200

    added polish translation

---
+++ lang-polish.js
@@ -0,0 +1,13 @@
+(function() {
+	module.exports.vortaro = function() {
+		return {
+			'1': ' razy powiedzieliśmy Siemankooooooooooooo!! Chory pojeb...',
+			'2': 'Cześć',
+			'3': 'cześć',
+			'4': 'Siemankooooooooooooo!!',
+			'5': 'Nie zwracam na Ciebie pieprzonej uwagi',
+			'6': 'Klub otwarty',
+			'7': 'Klub zamknięty'
+		};
+	};
+})();
