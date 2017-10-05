e2b5cd905ae62b4447815cefaa91cf6b9b66a1f0
Jacob Garby <j.garby@icloud.com>
2017-10-05 09:00:42 +0100

    Create lang-english.js

---
+++ lang-english.js
@@ -0,0 +1,13 @@
+﻿(function() {
+	module.exports.vortaro = function() {
+		return {
+			'1': 'It's time we said hellooooooooo! Putos enfermos...',
+			'2': 'Hi',
+			'3': 'hi',
+			'4': 'Helloooooooooo!!',
+			'5': 'A ti no te hago ni puto caso',
+			'6': 'Club abierto',
+			'7': 'Club cerrado'
+		};
+	};
+})();
