f7ec20600c504f6272bf849b47120d3bab9b3af8
braglin <fabiobraglin@gmail.com>
2017-10-06 13:01:53 -0300

    added portuguese translation
    Portuguese (PT-BR) translation file added

---
+++ lang-portuguese.js
@@ -0,0 +1,13 @@
+(function() {
+	module.exports.vortaro = function() {
+		return {
+			'1': ' vezes que dissemos Oiiiiiiiiiiiiiiii! Idiotas...',
+			'2': 'Oi',
+			'3': 'oi',
+			'4': 'Oiiiiiiiiiiiiiiii!!',
+			'5': 'Não estou nem aí com você!',
+			'6': 'Clube aberto',
+			'7': 'Clube fechado'
+		};
+	};
+})();
