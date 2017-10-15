932d4d41c48ccdfc7a268aae4204285b8c9f7f26
serk <marc.prat.maso@est.fib.upc.edu>
2017-10-15 13:55:08 +0200

    add:   + new eurobeat mix   + lenny faces   + repository link   + KAWAII === KANNA

--- index.js
+++ index.js
@@ -23,15 +23,27 @@ bot.on('/countholis', (ctx) => {
   ctx.reply.text(map.counterHolis + dict['1']);
 });
 bot.on('/eurobeat', (ctx) => {
-  ctx.reply.text('https://www.youtube.com/watch?v=7u3jv7zC4kU&list=PLGdEbnOoiEOOaFFYKh3A66wOUlrHUwzTs');
+  ctx.reply.text('https://www.youtube.com/watch?v=6ftCIfHwqtg');
+});
+bot.on('/slure', (ctx) => {
+  ctx.reply.text('¯\\_(ツ)_/¯');
+});
+bot.on('/slure', (ctx) => {
+  ctx.reply.text('( ͡° ͜ʖ ͡°)');
+});
+bot.on('/slure', (ctx) => {
+  ctx.reply.text('(╯°□°）╯︵ ┻━┻');
+});
+bot.on('/repository', (ctx) => {
+  ctx.reply.text('https://github.com/wolframtheta/BotCRUPC');
 });
 bot.on('/kawaiipotato', (ctx) => {
     bot.sendSticker(ctx.chat.id, 'CAADBAADkAADm9tZAcSYdB1cDI0jAg');
 });
+bot.on(/kawaii+/, (ctx) => kannaAtack(ctx));
 
 bot.on(/(^h|^H)i$/, (ctx) => holis(ctx));
 bot.on('sticker', (ctx) => {
-
   if (ctx.chat.id < 0 || (ctx.chat.id > 0 && ctx.from.username == 'XvaiKawaii')) {
     if (ctx.sticker.emoji == '🚪' && ctx.sticker.set_name == "CRUPC") {
       console.log("map:" + map.stateCRUPC);
@@ -51,7 +63,7 @@ bot.on('sticker', (ctx) => {
       console.log("closed");
       ctx.reply.text(dict['7']);
     }
-  } 
+  }
   else {
   ctx.reply.text(dict['5']);
   }
@@ -61,4 +73,22 @@ bot.start();
 function holis(ctx) {
   map.counterHolis++;
   ctx.reply.text(dict['4'])
+}
+
+function kannaAtack(ctx){
+  var kannaStickers = ["CAADBQADVAEAAhHsTwg9N6p2qMTE3wI","CAADBQADVQEAAhHsTwgIQ0YyGdeH9QI","CAADBQADWAEAAhHsTwj9HGLnMngW1QI",
+                       "CAADBQADVwEAAhHsTwicB3U_qK86oAI","CAADBQADVgEAAhHsTwhs3Z6OMn1UawI","CAADBQADUwEAAhHsTwhB0-MIi9feVAI",
+                       "CAADBQADUgEAAhHsTwg5I4hPQelh4gI","CAADBQADUQEAAhHsTwg5RdDBLjoHdQI","CAADBQADTgEAAhHsTwhpePGVn-6t9QI",
+                       "CAADBQADTwEAAhHsTwh70Gb8FU0DsgI","CAADBQADTQEAAhHsTwjahOlLCxcjIAI","CAADBQADUAEAAhHsTwj4FpK-IIMZ8QI",
+                       "CAADBQADTAEAAhHsTwiD24TUwFrgZgI","CAADBQADRQEAAhHsTwgJN6oJRfab5gI","CAADBQADRgEAAhHsTwjJlxDWHYKFagI",
+                       "CAADBQADSAEAAhHsTwiCvCzQ_OE7wQI","CAADBQADSgEAAhHsTwi8htmZ7HlbDwI","CAADBQADUAEAAhHsTwj4FpK-IIMZ8QI",
+                       "CAADBQADSQEAAhHsTwgIFSX340oseQI","CAADBQADSwEAAhHsTwgDcnQQAgYb8wI","CAADBQADRwEAAhHsTwgSs4dLAAEw0-YC",
+                       "CAADBQADRAEAAhHsTwizLpgWuol3egI","CAADBQADQwEAAhHsTwiLT7gXtD7hEgI","CAADBQADQgEAAhHsTwgZw93YJEw8pwI",
+                       "CAADBQADNQEAAhHsTwh2vqA8YAke1QI","CAADBQADNgEAAhHsTwgIkNgxKOiWtwI","CAADBQADNwEAAhHsTwhW_BdNqlTtsAI",
+                       "CAADBQADOAEAAhHsTwh3u4a5BYLc6gI","CAADBQADOwEAAhHsTwjTDnphc1JaMgI","CAADBQADPAEAAhHsTwhGoP0qNuDe7gI",
+                       "CAADBQADPQEAAhHsTwgtSje5XWFTaQI","CAADBQADPgEAAhHsTwiu4CgORjWQaQI","CAADBQADPwEAAhHsTwiK56bndPAnZwI",
+                       "CAADBQADQQEAAhHsTwhr-9Ep0-ZvwQI","CAADBQADQAEAAhHsTwhZdn81emDM1AI","CAADBQADNAEAAhHsTwiCsq4UgEk05QI",
+                       "CAADBQADMwEAAhHsTwgOGL07V0CZlAI","CAADBQADMgEAAhHsTwicRdiV65zXvQI","CAADBQADMQEAAhHsTwjyk_Fn-P_GKgI"];
+    var rand = Math.floor(Math.random() * kannaStickers.length) + 1;
+    bot.sendSticker(ctx.chat.id, kannaStickers[rand]).catch(function(){});
 }
