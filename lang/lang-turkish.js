220111450bace1188b5c7fbc4747c2aef289f893
onaralili <onerelili@gmail.com>
2017-10-24 23:03:19 +0200

    turkish language

---
+++ lang/lang-turkish.js
@@ -0,0 +1,13 @@
+﻿(function() {
+	module.exports.vortaro = function() {
+		return {
+			'1': 'Zamani geldi ve biz Merhabaaaaaa dedik! Hasta...',
+			'2': 'Merhaba',
+			'3': 'merhaba',
+			'4': 'Merhabaaaaaaaa!!',
+			'5': 'I do not do you a fucking affair',
+			'6': 'Açık kulüp',
+			'7': 'Kapalı kulüp'
+		};
+	};
+})();
