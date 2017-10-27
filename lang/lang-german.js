35f210890bac4da42dd17d2e1ca10ac008572213
Tim Fechner <tim.fechner@hs-hannover.de>
2017-10-27 11:08:55 +0200

    Add german translations

---
+++ lang/lang-german.js
@@ -0,0 +1,15 @@
+﻿(function() {
+	module.exports.vortaro = function() {
+		return {
+			'1': 'Endlich sagen wir halloooo!',
+			'2': 'Hey',
+			'3': 'Was geht',
+			'4': 'Halloooooooo!!',
+			'5': 'Ich tue dir keinen gefallen!',
+			'6': 'Offener Club',
+			'7': 'Geschlossener Club',
+            '8': 'Es gibt keine Tische zum Umschmeißen mehr',
+            '9': 'Es passen keine Tische mehr in den Club'
+		};
+	};
+})();
