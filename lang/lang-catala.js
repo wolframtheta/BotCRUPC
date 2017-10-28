88dfd5ed686362a4dfb8f08925bdaa967a3716fe
randprogrammer <33172166+randprogrammer@users.noreply.github.com>
2017-10-28 09:06:20 +0200

    Add Catalan Lang

---
+++ lang/lang-catala.js
@@ -0,0 +1,16 @@
+﻿
+(function() {
+	module.exports.vortaro = function() {
+		return {
+			'1': ' vegades hem dit Holiiiiiiiiiiiiiiii! Putos malalts...',
+			'2': 'Hi',
+			'3': 'hi',
+			'4': 'Holiiiiiiiiiiiiiiii!!',
+			'5': 'A tu no et faig ni puto cas',
+			'6': 'Club obert',
+			'7': 'Club tancat',
+            '8': 'No queden mes taules per tirar',
+            '9': 'No caben mes taules al despatx'
+		};
+	};
+})();
