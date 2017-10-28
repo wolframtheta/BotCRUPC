5780a60e2efda0eb8e2a9b47febb03425c195899
randprogrammer <33172166+randprogrammer@users.noreply.github.com>
2017-10-28 09:19:45 +0200

    Add Quenya Lang

---
+++ lang/lang-quenya.js
@@ -0,0 +1,16 @@
+﻿
+(function() {
+	module.exports.vortaro = function() {
+		return {
+			'1': ' lú emme quet Alatulyaaaaaaa! Nátyë necindo...',
+			'2': 'Alla',
+			'3': 'alla',
+			'4': 'Alatulyaaaaaaaaaa',
+			'5': 'Áva quetë',
+			'6': 'Ampano palime',
+			'7': 'Ampano cumna',
+            '8': 'Lala tare pálala',
+            '9': 'Lala tare ilmen'
+		};
+	};
+})();
