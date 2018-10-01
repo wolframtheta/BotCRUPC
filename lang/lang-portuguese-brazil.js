28b51dfb6ccda4cd2812b527af3d226133d05bca
João Pedro Sconetto <sconetto.joao@gmail.com>
2018-10-01 17:31:44 -0300

    Create lang-portugues-brazil.js
    Adds a new lang and correct some phrases from portuguese lang
    
    Solve issue #1

---
+++ lang/lang-portuguese-brazil.js
@@ -0,0 +1,15 @@
+(function() {
+	module.exports.vortaro = function() {
+		return {
+			'1': 'É tempo dizemos oláaaaaaaaaaaaaa! ',
+			'2': 'Oi',
+			'3': 'oi',
+			'4': 'Oláaaaaaaaaa!!',
+			'5': 'A você eu não faço nenhum caso',
+			'6': 'Clube aberto',
+			'7': 'Clube fechado',
+            '8': 'Não há mais mesas para virar',
+            '9': 'Mais nenhuma mesa cabe no clube'
+		};
+	};
+})();
