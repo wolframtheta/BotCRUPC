6b4c902d2dc229f1fd0a69ba63437df3529d1d54
Ralu <ralucris.v@gmail.com>
2018-10-27 04:27:31 +0200

    minor fixes

--- lang/lang-13375p34k.js
+++ lang/lang-13375p34k.js
@@ -2,15 +2,15 @@
 	module.exports.vortaro = function() {
 		return {
 			'lang_name': '13375P34K',
-			'hi_times': ' t1m3z w3 s41d H3LL0000!111!!1!!!!11!!1!!!! 51CK d00ds...',
+			'hi_times': ' t1m3z w3 s41d H3110000!111!!1!!!!11!!1!!!! 51CK d00ds...',
 			'hi_big': 'H1',
 			'hi_small': 'h1',
-			'hi_long': 'H3LL0000!111!!1!!!!11!!1!!!!',
+			'hi_long': 'H3110000!111!!1!!!!11!!1!!!!',
 			'no_admin': 'c3n50red, P00nd N008',
-			'open': 'C7ub 0P3N! w00t w00t!!!1!',
-			'closed': 'C7ub C7053D! g3t r3kt n00bs',
+			'open': 'C1U8 0P3N! w00t w00t!!!1!',
+			'closed': 'C1U8 C7053D! g3t r3kt n00bs',
     		'no_tables': 'N0 m0R t4bl35 2 r3k',
-    		'full_tables': 't3h p74c3 15 p4ck3D!1!! r3k s0m t4bl35 m4 b0ii',
+    		'full_tables': 't3h p14c3 i5 p4ck3D!1!! r3k s0m t4b135 m4 b0ii',
     		'enjoy_tune': 'nj0y t3h t00n m4 b0ii',
 		};
 	};
