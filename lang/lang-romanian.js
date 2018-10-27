3e84e27886eba333ebbb561f6519c9fa71a3c59b
Ralu <ralucris.v@gmail.com>
2018-10-27 04:00:31 +0200

    added translation to romanian language

---
+++ lang/lang-romanian.js
@@ -0,0 +1,17 @@
+(function() {
+	module.exports.vortaro = function() {
+		return {
+			'lang_name': 'Romana',
+			'hi_times': ' de ori am zis Bunaaaaaaaaaaaaaaaaa! Nebuni...',
+			'hi_big': 'Buna',
+			'hi_small': 'buna',
+			'hi_long': 'Bunaaaaaaaaaaaaaaaaa!!',
+			'no_admin': 'De tine n-ascult',
+			'open': 'Club deschis',
+			'closed': 'Club inchis',
+			'no_tables': 'Nu mai sunt mese pentru aruncat',
+			'full_tables': 'Nu mai incap mese in birou',
+			'enjoy_tune': 'Sa-ti fie de bine maneaua mea baiete'
+		};
+	};
+})();
