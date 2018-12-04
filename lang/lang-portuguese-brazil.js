3a8f7cd182c9cd7f4d201c6b1ccc885b05e53fcc
ctorres60 <carles.torres.garzo@est.fib.upc.edu>
2018-12-04 17:16:32 +0100

    mod playlist, rectificació llengues

--- lang/lang-portuguese-brazil.js
+++ lang/lang-portuguese-brazil.js
@@ -11,7 +11,7 @@
 			'closed': 'Clube fechado',
             'no_tables': 'Não há mais mesas para virar',
             'full_tables': 'Mais nenhuma mesa cabe no clube',
-			'enjoy_tune': 'Enjoy ma tune ma boi'
+			'enjoy_tune': 'Aproveite minha música meu bo'
 		};
 	};
 })();
